2f5e210853bc67d498264c173ee8f032115d1f5f
¨Davison <davisonfnx@gmail.com>
2019-03-26 15:54:51 -0300

    Aprendendo tipos de variaveis

---
+++ EstudosPHP/intruducaoPHP/tiposVariaveis.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Tipos basicos
+    $nome = "HCode";
+    $site = 'www.hcode.com.br';
+
+    $ano = 1990;
+    $salario = 5500.99;
+    $bloqueado = false;
+
+    //Composto e Tipos especiais
+    $frutas = array("abacaxi","laranja", "manga");
+
+    echo $frutas[1];
+
+    $nascimento = new DateTime();
+    echo "<br>";
+    //var_dump($nascimento);
+
+    $arquivo = fopen("tiposVariaveis.php", "r");
+    //var_dump($arquivo);
+
+    $nulo = null;
+    
+
+?>
