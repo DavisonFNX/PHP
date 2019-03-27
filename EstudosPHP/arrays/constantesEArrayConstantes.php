674a35fb5c9fb89f8531e7fd793d4bf10cac5e52
Davison Nascimento <davisonfnx@gmail.com>
2019-03-27 11:45:04 -0300

    Arrays, json e constantes

--- EstudosPHP/arrays/constantesEArrayConstantes.php
+++ EstudosPHP/arrays/constantesEArrayConstantes.php
@@ -2,4 +2,15 @@
 	define("SERVIDOR", "127.0.0.1");
 
 	echo SERVIDOR;
+
+	echo "";
+
+	define("BANCO_DE_DADOS", [
+		'127.0.0.1',
+		'root',
+		'password',
+		'teste'
+	]);
+
+	print_r(BANCO_DE_DADOS);
 ?>
