8fb8706139c66ed1fadb0dee08ee4bfb9f0b7bf5
¨Davison <davisonfnx@gmail.com>
2019-04-01 14:01:44 -0300

    Uso do recaptcha na seguranca

---
+++ seguranca/exemplo01.php
@@ -0,0 +1,8 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<form action="cadastrar.php" method="post">
+	<div class="g-recaptcha" data-sitekey="6LfmYJsUAAAAACvMbcPXS_KqLYj2LrIFksmwdfHY"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+
+</form>
