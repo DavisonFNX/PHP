cc4c9fe2458d9cdd8e98aeea98c09adf91eca9b1
¨Davison <davisonfnx@gmail.com>
2019-04-01 15:56:58 -0300

    Pasta do curso PHP Fundamental

---
+++ php_fundamental/unidade_04/inicial.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    	<!-- Abaixo teste de echo -->
+    	<p><h1><?php 
+    	//comentario nao sera visto no browser
+    	echo "Davison Nascimento" 
+
+    	?></h1></p>
+    </body>
+</html>
